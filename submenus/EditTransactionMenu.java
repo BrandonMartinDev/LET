28c1ce5ace260318dc9b81399ac15bcf82926158
Brandon Martin <SomeBusinessEmail@protonmail.com>
2026-07-05 15:05:29 -0400

    Added editing transaction functionality

--- submenus/EditTransactionMenu.java
+++ submenus/EditTransactionMenu.java
@@ -18,7 +18,58 @@ public class EditTransactionMenu extends SubMenu { // Class that extends SubMenu
 
     // -- == [[ METHODS ]] == -- \\
 
-    private LocalDate promptForDate() {
+    private int promptForTransactionIDToEdit() {
+
+        // Prompts user for transaction id to remove
+
+        System.out.println();
+        System.out.print("Transaction ID to edit: ");
+
+        try {
+
+            // Reads integer from scanner and discards rest of tokens
+
+            int input = scnr.nextInt();
+            scnr.nextLine();
+
+            // Verifies number is 7 digits long and is a valid transaction id
+
+            if (input < 1000000 || input > 9999999) {
+                throw new Error("Please provide a valid 7 digit number!");
+            }
+
+            if (Transaction.TransactionList.getOrDefault(input, null) == null) {
+                throw new Error("Transaction ID does not exist!");
+            }
+
+            // Returns input
+
+            return input;
+
+        } catch (InputMismatchException e) {
+
+            // If user inputs anything other than an integer
+            // Warns user
+            // Discards tokens
+            // Reprompts for user to try again
+
+            System.out.println("Please provide a valid 7 digit number!");
+            scnr.nextLine();
+            return promptForTransactionIDToEdit();
+
+        } catch (Error e) {
+
+            // If an error occurs
+            // Warns user and reprompts for user to try again
+
+            System.out.println(e.getMessage());
+            return promptForTransactionIDToEdit();
+
+        }
+
+    }
+
+    private LocalDate promptForNewDate() {
 
         // Initializes input variable
 
@@ -28,7 +79,7 @@ public class EditTransactionMenu extends SubMenu { // Class that extends SubMenu
 
         System.out.println();
         System.out.println("Date must be in format 'YYYY-MM-DD'");
-        System.out.print("Date: ");
+        System.out.print("New Date: ");
 
         // Attempts to read input from scanner
 
@@ -43,7 +94,7 @@ public class EditTransactionMenu extends SubMenu { // Class that extends SubMenu
 
             System.out.println("Please enter the date as text in the valid format!");
             scnr.nextLine();
-            return promptForDate();
+            return promptForNewDate();
 
         }
 
@@ -56,24 +107,24 @@ public class EditTransactionMenu extends SubMenu { // Class that extends SubMenu
 
         if (input.isBlank()) {
             System.out.println("\"Date\" cannot be blank!");
-            return promptForDate();
+            return promptForNewDate();
         }
 
         if (!Transaction.isDateStringValid(input)) {
             System.out.println("\"Date\" is in an invalid format!");
-            return promptForDate();
+            return promptForNewDate();
         }
 
         return Transaction.getDateFromString(input);
 
     }
 
-    private double promptForAmount() {
+    private double promptForNewAmount() {
 
         // Prompts user for transaction amount
 
         System.out.println();
-        System.out.print("Amount: ");
+        System.out.print("New Amount: ");
 
         try {
 
@@ -102,7 +153,7 @@ public class EditTransactionMenu extends SubMenu { // Class that extends SubMenu
 
             System.out.println("Please provide a valid number greater than or equal to 0!");
             scnr.nextLine();
-            return promptForAmount();
+            return promptForNewAmount();
 
         } catch (Error e) {
 
@@ -111,12 +162,12 @@ public class EditTransactionMenu extends SubMenu { // Class that extends SubMenu
             // Reprompt user to try again
 
             System.out.println(e.getMessage());
-            return promptForAmount();
+            return promptForNewAmount();
         }
 
     }
 
-    private TransactionType promptForType() {
+    private TransactionType promptForNewType() {
 
         // Initializes input variable
 
@@ -126,7 +177,7 @@ public class EditTransactionMenu extends SubMenu { // Class that extends SubMenu
 
         System.out.println();
         System.out.println("Type must either be 'DONATION' or 'EXPENSE'");
-        System.out.print("Transaction Type: ");
+        System.out.print("New Transaction Type: ");
 
         // Attempts to read input from scanner
 
@@ -141,7 +192,7 @@ public class EditTransactionMenu extends SubMenu { // Class that extends SubMenu
 
             System.out.println("Please enter the type as text!");
             scnr.nextLine();
-            return promptForType();
+            return promptForNewType();
 
         }
 
@@ -154,12 +205,12 @@ public class EditTransactionMenu extends SubMenu { // Class that extends SubMenu
 
         if (input.isBlank()) {
             System.out.println("\"Transaction Type\" cannot be blank!");
-            return promptForType();
+            return promptForNewType();
         }
 
         if (!Transaction.isTransactionTypeStringValid(input)) {
             System.out.println("\"Type\" is not 'DONATION' or 'EXPENSE'!");
-            return promptForType();
+            return promptForNewType();
         }
 
         return Transaction.getTransactionTypeFromString(input);
@@ -173,37 +224,121 @@ public class EditTransactionMenu extends SubMenu { // Class that extends SubMenu
         System.out.print("\033[H\033[2J");
         printMenuName();
 
-        // Gets transaction info from user input
+        // Prompt user whether to display the transaction list or not
 
-        String title = promptForString("Title");
-        String description = promptForString("Description");
-        LocalDate date = promptForDate();
-        double amount = promptForAmount();
-        TransactionType type = promptForType();
+        boolean displayTransactionList = promptToDisplayTransactionList();
+
+        if (displayTransactionList) {
+            System.out.println();
+            Transaction.displayCurrentTransactionList();
+        } else {
+            System.out.println("Skipping displaying transaction list");
+        }
+
+        // Gets transaction id from user input
+
+        int idToEdit = promptForTransactionIDToEdit();
+        Transaction transactionToEdit = Transaction.TransactionList.getOrDefault(idToEdit, null);
+
+        // Guard clause that protects against invalid transaction id provided
+
+        if (transactionToEdit == null) {
+            System.out.println("Transaction ID does not exist!");
+            promptToSwitchToMainMenu();
+            return;
+        }
+
+        // Prompts user to confirm editing transaction
+
+        System.out.println("Editing '" + transactionToEdit.getTitle() + "'.");
+        boolean confirmEditing = promptToConfirmOption();
+
+        // If user denied editing transaction
+        // Tell user editing was canceled and re-prompt to go to main menu
+
+        System.out.print("\033[H\033[2J");
+        printMenuName();
+
+        if (confirmEditing != true) {
+
+            System.out.println(
+                    "Canceled editing '" + transactionToEdit.getTitle() + "' (" + transactionToEdit.getId() + ")");
+            System.out.println();
+
+            promptToSwitchToMainMenu();
+            return;
+
+        }
+
+        // Gets new transaction info from user input
+
+        System.out.println("Current Title: " + transactionToEdit.getTitle());
+        String newTitle = promptForString("New Title");
+
+        System.out.print("\033[H\033[2J");
+        printMenuName();
+        System.out.println("Current Description: " + transactionToEdit.getDescription());
+        String newDescription = promptForString("New Description");
+
+        System.out.print("\033[H\033[2J");
+        printMenuName();
+        System.out.println("Current Date: " + transactionToEdit.getDate());
+        LocalDate newDate = promptForNewDate();
+
+        System.out.print("\033[H\033[2J");
+        printMenuName();
+        System.out.println("Current Amount: " + transactionToEdit.getAmount());
+        double newAmount = promptForNewAmount();
+
+        System.out.print("\033[H\033[2J");
+        printMenuName();
+        System.out.println("Current Type: " + transactionToEdit.getType());
+        TransactionType newType = promptForNewType();
 
         // Clears console again and creates new Transaction from user provided info
 
         System.out.print("\033[H\033[2J");
+        printMenuName();
+
         Transaction newTransaction;
 
         try {
-            newTransaction = new Transaction(title, description, date, amount,
-                    type);
+            newTransaction = new Transaction(newTitle, newDescription, newDate, newAmount, newType);
+            newTransaction.setId(idToEdit);
         } catch (Exception e) {
-            System.out.println("There was an error creating transaction, please try again...");
+            System.out.println("There was an error editing transaction, please try again...");
             promptToSwitchToMainMenu();
             return;
         }
 
         // Tells users new transaction was created successfully
 
-        System.out.println("\nCreated new transaction successfully!\n");
+        System.out.println("New Transaction Info:");
+        System.out.println();
         System.out.println(newTransaction);
+        System.out.println();
 
-        // Adds transaction to list and tell user
+        // Ask for user confirmation to edit transaction info
 
-        Transaction.TransactionList.put(newTransaction.getId(), newTransaction);
-        System.out.println("\nAdded transaction to list\n");
+        boolean confirmNewInfo = promptToConfirmOption();
+
+        if (confirmNewInfo != true) {
+
+            System.out.println(
+                    "Canceled editing '" + transactionToEdit.getTitle() + "' (" + transactionToEdit.getId() + ")");
+            System.out.println();
+
+            promptToSwitchToMainMenu();
+            return;
+
+        }
+
+        // Updates transaction in TransactionList hashmap and tells user transaction was
+        // updated successfully
+
+        Transaction.TransactionList.put(idToEdit, newTransaction);
+        System.out.println(
+                "Edited '" + transactionToEdit.getTitle() + "' (" + transactionToEdit.getId() + ") successfully!");
 
         // Prompt to switch back to main menu
 
